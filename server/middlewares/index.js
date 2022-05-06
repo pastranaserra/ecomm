a04c3211e0571a537fa09fbb210ab69196c5216a
Karlo Verde <karlo.verde.1996@gmail.com>
2022-05-06 00:29:31 -0500

    chore: expose paginator middleware

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,9 +1,11 @@
+const errorHandlers = require('./error');
+const { paginator } = require('./paginator');
 const reqIdSetter = require('./req-id-setter');
 const reqLogger = require('./logger');
-const errorHandlers = require('./error');
 
 module.exports = {
   reqIdSetter,
   reqLogger,
+  paginator,
   errorHandlers,
 };
