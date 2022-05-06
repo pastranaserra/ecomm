749a3e898b9921a55b60024e394eb65eccd42c09
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2022-05-06 00:31:25 -0500

    chore: create paginator middleware (#8)

---
+++ server/middlewares/paginator.js
@@ -0,0 +1,61 @@
+const defaultLimit = 10;
+const minLimit = 1;
+const maxLimit = 100;
+
+const defaultOffset = 0;
+const minOffset = 0;
+
+exports.paginator = (req, _, next) => {
+  const {
+    limit: limitStr = defaultLimit, // This comment prevents linting issues.
+    offset: offsetStr = defaultOffset,
+  } = req.query;
+  const limit = parseInt(limitStr, 10);
+  const offset = parseInt(offsetStr, 10);
+  const limitIsValid = !Number.isNaN(limit) && limit >= minLimit;
+  const offsetIsValid = !Number.isNaN(offset) && offset >= minOffset;
+  if (!limitIsValid || !offsetIsValid) {
+    const baseErrorMsg = 'Bad request: Invalid pagination parameters';
+    const invalidLimitMsg = limitIsValid
+      ? ''
+      : ' `limit` must be a positive integer.';
+    const invalidOffsetMsg = offsetIsValid
+      ? ''
+      : ' `offset` must be an integer greater than or equal to 0.';
+    next({
+      statusCode: 400,
+      message: `${baseErrorMsg}${invalidLimitMsg}${invalidOffsetMsg}`,
+    });
+  }
+  req.limit = Math.min(limit, maxLimit);
+  req.offset = offset;
+  next();
+};
+
+exports.paginatorQueryParamsDocs = {
+  limitQueryParam: {
+    in: 'query',
+    name: 'limit',
+    description: 'Number of items to return',
+    required: false,
+    schema: {
+      type: 'integer',
+      format: 'int32',
+      minimum: minLimit,
+      maximum: maxLimit,
+      default: defaultLimit,
+    },
+  },
+  offsetQueryParam: {
+    in: 'query',
+    name: 'offset',
+    description: 'Number of items to skip',
+    required: false,
+    schema: {
+      type: 'integer',
+      format: 'int32',
+      minimum: minOffset,
+      default: defaultOffset,
+    },
+  },
+};
