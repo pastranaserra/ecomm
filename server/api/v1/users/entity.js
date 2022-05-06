4fdddc270987306d82299505e59070a08afe15eb
Karlo Verde <karlo.verde.1996@gmail.com>
2022-05-06 00:49:47 -0500

    chore: create user model/schema

---
+++ server/api/v1/users/entity.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
+const explicitFields = {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: (value) => isEmail(value),
+      message: () => 'Invalid email',
+    },
+  },
+};
+
+const userSchema = new mongoose.Schema(explicitFields, { timestamps: true });
+
+module.exports = {
+  User: mongoose.model('User', userSchema),
+};
