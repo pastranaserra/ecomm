91ebdd87ee0494d34a7d4b7ac448b5446b7e3841
Karlo Verde <karlo.verde.1996@gmail.com>
2022-05-06 01:03:49 -0500

    feat: add password field (hidden on reads)

--- server/api/v1/users/entity.js
+++ server/api/v1/users/entity.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
 
+const hiddenFields = {
+  password: {
+    type: String,
+    required: true,
+  },
+};
+
 const explicitFields = {
   name: {
     type: String,
@@ -29,9 +36,18 @@ const explicitFields = {
     type: Boolean,
     default: false,
   },
+  ...hiddenFields,
 };
 
 const userSchema = new mongoose.Schema(explicitFields, { timestamps: true });
+
+// eslint-disable-next-line func-names
+userSchema.methods.toJSON = function () {
+  const userJson = this.toObject();
+  const hiddenFieldNames = Object.keys(hiddenFields);
+  hiddenFieldNames.forEach((fieldName) => delete userJson[fieldName]);
+  return userJson;
+};
 
 module.exports = {
   User: mongoose.model('User', userSchema),
