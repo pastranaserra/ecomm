a1979cad49ca1b6fb9684f7da506dbe6aa4ede42
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2022-05-07 18:05:17 -0500

    feat: create default admin if none (#13)

--- config.js
+++ config.js
@@ -8,6 +8,12 @@ const config = {
     usernane: process.env.DB_USERNANE,
     password: process.env.DB_PASSWORD,
   },
+  defaultAdmin: {
+    name: process.env.DEFAULT_ADMIN_NAME,
+    lastName: process.env.DEFAULT_ADMIN_LAST_NAME,
+    email: process.env.DEFAULT_ADMIN_EMAIL,
+    password: process.env.DEFAULT_ADMIN_PASSWORD,
+  },
   jwt: {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN,
