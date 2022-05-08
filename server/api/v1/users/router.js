6187ae9be7f1dcc5e735b9706b967dd775f71291
Karlo Verde <karlo.verde.1996@gmail.com>
2022-05-07 23:19:44 -0500

    feat: expose users list to admins only

--- server/api/v1/users/router.js
+++ server/api/v1/users/router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const { paginator } = require('../../../middlewares/paginator');
-const { me } = require('../auth/middlewares');
+const { me, isAdmin } = require('../auth/middlewares');
 const { listUsers } = require('./controller');
 
 const router = express.Router();
 
-router.route('/').get(me, paginator, listUsers);
+router.route('/').get(me, isAdmin, paginator, listUsers);
 
 module.exports = router;
