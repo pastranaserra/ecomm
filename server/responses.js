9dc84b3ab3eb2964fc80cdc77118e44997c4490c
Karlo Verde <karlo.verde.1996@gmail.com>
2022-05-07 20:44:35 -0500

    chore: create error response builders

---
+++ server/responses.js
@@ -0,0 +1,24 @@
+exports.BadRequestErrorResponse = (message = 'Bad Request') => ({
+  status: 400,
+  message,
+});
+
+exports.UnauthorizedErrorResponse = (message = 'Unauthorized') => ({
+  statusCode: 401,
+  message,
+});
+
+exports.ForbiddenErrorResponse = (message = 'Forbidden') => ({
+  statusCode: 403,
+  message,
+});
+
+exports.NotFoundErrorResponse = (message = 'Not Found') => ({
+  statusCode: 404,
+  message,
+});
+
+exports.InternalServerErrorResponse = (message = 'Internal Server Error') => ({
+  statusCode: 500,
+  message,
+});
