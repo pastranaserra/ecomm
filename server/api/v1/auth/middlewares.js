92a5ff45ace84ef128719ad38d4375b725d1616c
Karlo Verde <karlo.verde.1996@gmail.com>
2022-05-07 20:51:24 -0500

    refactor: use response builders

--- server/api/v1/auth/middlewares.js
+++ server/api/v1/auth/middlewares.js
@@ -3,12 +3,7 @@ const { verify } = require('jsonwebtoken');
 const {
   jwt: { secret },
 } = require('../../../../config');
-const logger = require('../../../../logger');
-const {
-  UnauthorizedErrorResponse,
-  ForbiddenErrorResponse,
-  InternalServerErrorResponse,
-} = require('../../../responses');
+const { UnauthorizedErrorResponse } = require('../../../responses');
 const { User } = require('../users');
 
 const bearerPrefix = 'Bearer ';
@@ -27,24 +22,6 @@ exports.me = async (req, _, next) => {
     const me = await User.findById(id);
     if (!me) return next(UnauthorizedErrorResponse());
     req.me = me;
-    return next();
-  } catch (err) {
-    return next(err);
-  }
-};
-
-exports.isAdmin = async (req, _, next) => {
-  try {
-    if (!req.me) {
-      logger.error(
-        `
-Implementation error:
-The 'me' middleware must be used before the 'isAdmin' middleware.
-`,
-      );
-      return next(InternalServerErrorResponse());
-    }
-    if (!req.me.isAdmin) return next(ForbiddenErrorResponse());
     return next();
   } catch (err) {
     return next(err);
