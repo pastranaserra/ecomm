1f844c0b31c5e69a5128386cf21417ef0b747565
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2022-07-01 18:23:36 -0500

    refactor: isolate pagination params getter (#21)

--- server/api/v1/users/controller.js
+++ server/api/v1/users/controller.js
@@ -1,19 +1,9 @@
-const logger = require('../../../../logger');
-const { InternalServerErrorResponse } = require('../../../responses');
+const { getPaginationParams } = require('../../../middlewares/paginator');
 const { User } = require('./entity');
 
 exports.listUsers = async (req, res, next) => {
   try {
-    const { limit, offset } = req;
-    if (Number.isNaN(limit) || Number.isNaN(offset)) {
-      logger.error(
-        `
-Implementation error:
-The 'paginator' middleware must be used before the 'listUsers' handler.
-`,
-      );
-      return next(InternalServerErrorResponse());
-    }
+    const { limit, offset } = getPaginationParams(req);
     const [users, usersCount] = await Promise.all([
       User.find().skip(offset).limit(limit),
       User.countDocuments(),
